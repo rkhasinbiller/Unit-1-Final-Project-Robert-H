209164fda6286d50a5f08ab7b30c2dae70d3db6e
Robert Hasinbiller <rkhasinbiller@gmail.com>
2025-07-15 20:26:01 -0500

    Added Footer page

---
+++ final-project/src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer() {
+  return(
+    <footer>
+      <p>&copy; {new Date().getFullYear()} Your website name. All Rights Reserved</p>
+    </footer>
+  )
+}
+
+export default Footer
