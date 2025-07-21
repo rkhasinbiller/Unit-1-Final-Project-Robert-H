2d464d4f573e709d6e3a3177d1cbd31b371e5eea
Robert Hasinbiller <rkhasinbiller@gmail.com>
2025-07-20 23:58:59 -0500

    added timer component

--- final-project/src/components/Timer.jsx
+++ final-project/src/components/Timer.jsx
@@ -1,2 +1,9 @@
 import { useState, useEffect, useRef } from "react";
 import "./Timer.css";
+
+export default function Timer({ onSessionStart }) {
+  const [timeLeft, setTimeLeft] = useState(25 * 60); // 25 minutes
+  const [isRunning, setIsRunning] = useState(false);
+  const timerRef = useRef(null);
+
+export default function Timer
