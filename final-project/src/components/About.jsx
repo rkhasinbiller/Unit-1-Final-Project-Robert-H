9669959b2a64773b45e84bee7c7fad82b8eddecf
Robert Hasinbiller <rkhasinbiller@gmail.com>
2025-07-22 00:56:58 -0500

    about.jsx

---
+++ final-project/src/components/About.jsx
@@ -0,0 +1,14 @@
+export default function About() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>About This App</h2>
+      <p>
+        The Pomodoro App is timer that helps you stay productive by breaking your work into 25-minute focus sessions. 
+        After each session, take a short break and come back refreshed.
+      </p>
+      <p>
+        You’ll also get motivational quotes to keep your spirits high while working hard!
+      </p>
+    </div>
+  );
+}
