3fe9c33c0beb2ca607105bf4179ee05da446a98e
Robert Hasinbiller <rkhasinbiller@gmail.com>
2025-07-22 01:18:01 -0500

    button

---
+++ final-project/src/pages/Favorites.jsx
@@ -0,0 +1,8 @@
+export default function Favorites() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>Favorites</h2>
+      <p>Feature coming soon! Here you'll be able to save your favorite motivational quotes.</p>
+    </div>
+  );
+}
