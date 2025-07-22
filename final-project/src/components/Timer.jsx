ce989cf47adcaf6ff6f0cf4e1a01bcccce2cfc96
Robert Hasinbiller <rkhasinbiller@gmail.com>
2025-07-22 00:04:12 -0500

    added motivational quote

--- final-project/src/components/Timer.jsx
+++ final-project/src/components/Timer.jsx
@@ -7,3 +7,19 @@ function App() {
   const intervalRef = useRef(null);               // Timer ID
 
 }
+
+  // Motivational quote 
+  function fetchQuote() {
+  fetch("https://corsproxy.io/?https://zenquotes.io/api/random")
+    .then((res) => res.json())
+    .then((data) => {
+      const quote = data[0];
+      setQuote({
+        content: quote.q,
+        author: quote.a,
+      });
+    })
+    .catch((err) => {
+      console.error("Failed to fetch quote:", err);
+    });
+}
