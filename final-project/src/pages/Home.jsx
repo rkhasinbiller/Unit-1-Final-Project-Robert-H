ae210129b10707f6c556565d21894a5557f8581d
Robert Hasinbiller <rkhasinbiller@gmail.com>
2025-07-24 02:26:54 -0500

    upload

--- final-project/src/pages/Home.jsx
+++ final-project/src/pages/Home.jsx
@@ -7,8 +7,11 @@ export default function Home() {
     <div>
       <h2>Welcome to The Pomodoro App</h2>
       <p>This app helps you stay productive with 25-minute work intervals.</p>
-<Form />
-      <Timer />
+
+      <div className="layout">
+        <Form />
+        <Timer />
+      </div>
     </div>
   );
 }
