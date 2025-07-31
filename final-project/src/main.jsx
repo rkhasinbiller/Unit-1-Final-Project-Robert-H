76f0731e4407f573879e0c1afea07eb1087d5cdc
Robert Hasinbiller <rkhasinbiller@gmail.com>
2025-07-30 22:07:50 -0500

    test

--- final-project/src/main.jsx
+++ final-project/src/main.jsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client' //react-dom
 import './index.css'
 import App from './App.jsx'
 
@@ -8,3 +8,5 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+
--- final-project/vite.config.js
+++ final-project/vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: { // <--- Add this 'build' object
-    rollupOptions: { // <--- Add this 'rollupOptions' object
-      external: ['react-router-dom'] // <--- Add this 'external' array
-    }
-  }
 });
